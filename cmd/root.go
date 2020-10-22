9b610ffec8e766ca75912e0902afd5eba85d5cf9
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-22 22:07:17 +0100

    Add root cmd

---
+++ cmd/root.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "taskEr",
+	Short: "CLI task manager",
+	Long:  "taskEr is my very own CLI task manager",
+	//Run:
+}
