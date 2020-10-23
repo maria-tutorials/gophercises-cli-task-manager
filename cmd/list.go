7638c9b61ba2a78115d28965d961a4944178dd70
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-23 22:58:21 +0100

    Make list work using bold

--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+
+	"../db"
 
 	"github.com/spf13/cobra"
 )
@@ -14,6 +17,20 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list prints all the tasks that need to be done",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("here they are")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			log.Fatal("ups something went wrong", err)
+		}
+
+		if len(tasks) == 0 {
+			fmt.Println("There's no tasks, you're all free")
+			return
+		}
+
+		fmt.Println("You have the following tasks:")
+		for i, t := range tasks {
+			fmt.Printf("%d. %s\n", (i + 1), t.Value)
+		}
+
 	},
 }
