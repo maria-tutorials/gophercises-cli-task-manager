408d339e83c34d692a7b93675adad8a3eb406d56
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-23 22:39:46 +0100

    Add tasks into buckets

--- db/tasks.go
+++ db/tasks.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"log"
 	"time"
 
@@ -15,6 +16,7 @@ type Task struct {
 	Value string
 }
 
+// Init connects to bold and creates a new bucket if needed
 func Init(path string) error {
 	var err error
 	db, err = bolt.Open(path, 0600, &bolt.Options{Timeout: 1 * time.Second})
@@ -26,4 +28,30 @@ func Init(path string) error {
 		_, err := tx.CreateBucketIfNotExists(tasksBucket)
 		return err
 	})
+}
+
+// CreateTask adds a new task to the database
+func CreateTask(task string) (int, error) {
+	id := 0
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(tasksBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(64))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
